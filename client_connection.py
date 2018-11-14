ddc515e22ffab201bcbc675db09b9489e8445d0f
a-shakra <shakra.anas@gmail.com>
2018-11-13 21:02:32 -0500

    standard incremental update

--- client_connection.py
+++ client_connection.py
@@ -1,5 +1,16 @@
 import ast
 import threading
+from utils import (dict_to_bytes,
+                   name_registered,
+                   update_txt_file,
+                   recover_state,
+                    top_bidder,
+                   name_matches_ip,
+                   has_open_items,
+                   under_three_opens,
+                   client_connected,
+                   is_ip,
+                   getItemDescriptions)
 # referenced here: https://www.techbeamers.com/python-tutorial-write-multithreaded-python-server/
 
 
@@ -24,8 +35,18 @@ class ClientConnection(threading.Thread):
             if not data:
                 #print("Data is not correct")
                 break
-            #data = ast.literal_eval(data)
-            print("Message received from client over tcp: {}".format(data))
+            msg_received = ast.literal_eval(data)
+            print("Message received from client over tcp: {}".format(data)) # For testing purposes
+            return_msg = self.handle_response(msg_received)
+            return_msg = dict_to_bytes(return_msg)
+            self.send_msg(return_msg)
+
+    def handle_response(self, msg_received):
+        #Message will always be a bid. What this function will do is check whether to send the Winner and
+        #the highest bid messages, in addition to the general bid confirmed message
+        print('hello')
+        msg = 'hello'
+        return msg
 
     def send_msg(self, msg):
         """msg: a dict to be sent to the client"""
